4f2725bde878e3017d8041cc15fe43af76f42c13
Peter Petrovics <szerintedmi@gmail.com>
2017-12-18 16:07:44 +0000

    graph color tweaks

--- src/lib/graphs.js
+++ src/lib/graphs.js
@@ -1,20 +1,39 @@
 'use strict';
 const Chart = require('chart.js');
-const DARKGREEN = 'rgba(3, 71, 50, 1)';
+const PURPLE = 'rgba(139, 95, 191, 1)';
+//const DARKGREEN = 'rgba(3, 71, 50, 1)';
 const GREEN = 'rgba(0, 129, 72, 1)';
 const YELLOW = 'rgba(198, 192, 19, 1)';
 const ORANGE = 'rgba(239, 138, 23, 1)';
-const RED = 'rgba(239, 41, 23, 1)';
+//const RED = 'rgba(239, 41, 23, 1)';
+const BLUE = 'rgba(41, 51, 155, 1)';
+
 const DARKRED = 'rgba(141, 8, 1, 1)';
 
+const PURPLE_OPA = 'rgba(139, 95, 191, 0.2)';
 const GREY_OPA = 'rgb(230, 88, 88)';
 const TRANSPARENT = 'rgba(0, 0, 0, 0)';
-const DARKGREEN_OPA = 'rgba(3, 71, 50, 0.2)';
+//const DARKGREEN_OPA = 'rgba(3, 71, 50, 0.2)';
 const GREEN_OPA = 'rgba(0, 129, 72,  0.2)';
 const YELLOW_OPA = 'rgba(198, 192, 19,  0.2)';
 const ORANGE_OPA = 'rgba(239, 138, 23,  0.2)';
-const RED_OPA = 'rgba(239, 41, 23,  0.2)';
+//const RED_OPA = 'rgba(239, 41, 23,  0.2)';
 const DARKRED_OPA = 'rgba(141, 8, 1,  0.2)';
+const BLUE_OPA = 'rgba(41, 51, 155, 0.2)';
+
+const ACD_SUPPLY_COLOR = BLUE;
+const ACD_SUPPLY_COLOR_OPA = BLUE_OPA;
+const LOCKED_ACD_COLOR = YELLOW;
+const LOCKED_ACD_COLOR_OPA = YELLOW_OPA;
+const USER_ACD_COLOR = ORANGE;
+const USER_ACD_COLOR_OPA = ORANGE_OPA;
+const SYSTEM_ACC_COLOR = GREEN;
+const SYSTEM_ACC_COLOR_OPA = GREEN_OPA;
+const DEFAULTED_COLOR = DARKRED;
+const DEFAULTED_COLOR_OPA = DARKRED_OPA;
+const OPEN_LOANS_COLOR = PURPLE;
+const OPEN_LOANS_COLOR_OPA = PURPLE_OPA;
+
 const ONE_DAY_IN_SECS = 24 * 60 * 60;
 
 // prettier-ignore
@@ -47,7 +66,11 @@ const graphs = [
     {
         title: 'Total ACD',
         datasets: [{
-            func: augmint => { return Math.round(augmint.totalAcd); }
+            func: augmint => { return Math.round(augmint.totalAcd); },
+            options: {
+                borderColor: ACD_SUPPLY_COLOR,
+                backgroundColor: ACD_SUPPLY_COLOR_OPA
+            }
         }]
     },
     {
@@ -63,16 +86,16 @@ const graphs = [
                 func: augmint => { return (augmint.systemAcd); },
                 options: {
                     label: 'system',
-                    borderColor: DARKGREEN,
-                    backgroundColor: DARKGREEN
+                    borderColor: SYSTEM_ACC_COLOR,
+                    backgroundColor: SYSTEM_ACC_COLOR
                 }
             },
             {
                 func: augmint => { return augmint.balances.lockedAcdPool; },
                 options: {
                     label: 'locked',
-                    borderColor: YELLOW,
-                    backgroundColor: YELLOW
+                    borderColor: LOCKED_ACD_COLOR,
+                    backgroundColor: LOCKED_ACD_COLOR
                 }
             },
             {
@@ -81,8 +104,8 @@ const graphs = [
                 },
                 options: {
                     label: 'user',
-                    borderColor: RED,
-                    backgroundColor: RED
+                    borderColor: USER_ACD_COLOR,
+                    backgroundColor: USER_ACD_COLOR
                 }
             }
         ]
@@ -90,56 +113,93 @@ const graphs = [
     {
         title: 'users\' ACD (accs + orders)',
         datasets: [{
-            func: augmint => { return augmint.usersAcd; }
+            func: augmint => { return augmint.usersAcd; },
+            options: {
+                label: 'user',
+                borderColor: USER_ACD_COLOR,
+                backgroundColor: USER_ACD_COLOR_OPA
+            }
         }]
     },
     {
         title: 'ACD Reserves',
         datasets: [{
-            func: augmint => { return augmint.reserveAcd; }
+            func: augmint => { return augmint.reserveAcd; },
+            options: {
+                borderColor: SYSTEM_ACC_COLOR,
+                backgroundColor: SYSTEM_ACC_COLOR_OPA
+            }
         }]
     },
     {
         title: 'ETH Reserves',
         datasets: [{
-            func: augmint => { return augmint.reserveEth; }
+            func: augmint => { return augmint.reserveEth; },
+            options: {
+                borderColor: SYSTEM_ACC_COLOR,
+                backgroundColor: SYSTEM_ACC_COLOR_OPA
+            }
         }]
     },
     {
         title: 'Interest Earned (ACD)',
         datasets: [{
-            func: augmint => { return augmint.balances.interestEarnedPool; }
+            func: augmint => { return augmint.balances.interestEarnedPool; },
+            options: {
+                borderColor: SYSTEM_ACC_COLOR,
+                backgroundColor: SYSTEM_ACC_COLOR_OPA
+            }
         }]
     },
     {
         title: 'ACD Locked',
         datasets: [{
-            func: augmint => { return augmint.balances.lockedAcdPool; }
+            func: augmint => { return augmint.balances.lockedAcdPool; },
+            options: {
+                    borderColor: LOCKED_ACD_COLOR,
+                    backgroundColor: LOCKED_ACD_COLOR_OPA
+                }
         }]
     },
     {
         title: 'ACD in open Loans',
         datasets: [{
-            func: augmint => { return augmint.balances.openLoansAcd; }
+            func: augmint => { return augmint.balances.openLoansAcd; },
+            options: {
+                    borderColor: OPEN_LOANS_COLOR,
+                    backgroundColor: OPEN_LOANS_COLOR_OPA
+                }
         }]
     },
     {
         title: 'Total default Loans (ACD)',
         datasets: [{
-            func: augmint => { return augmint.balances.defaultedLoansAcd; }
+            func: augmint => { return augmint.balances.defaultedLoansAcd; },
+            options: {
+                borderColor: DEFAULTED_COLOR,
+                backgroundColor: DEFAULTED_COLOR_OPA
+            }
         }]
     },
 
     {
         title: 'ACD fees earned',
         datasets: [{
-            func: augmint => { return augmint.balances.acdFeesEarned; }
+            func: augmint => { return augmint.balances.acdFeesEarned; },
+            options: {
+                borderColor: SYSTEM_ACC_COLOR,
+                backgroundColor: SYSTEM_ACC_COLOR_OPA
+            }
         }]
     },
     {
         title: 'ETH fees earned',
         datasets: [{
-            func: augmint => { return augmint.balances.ethFeesEarned; }
+            func: augmint => { return augmint.balances.ethFeesEarned; },
+            options: {
+                borderColor: SYSTEM_ACC_COLOR,
+                backgroundColor: SYSTEM_ACC_COLOR_OPA
+            }
         }]
     }
 ];
