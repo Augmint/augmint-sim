7d380afe39799e86bb112ec7e8b98b419aa472c9
Laszlo Barjak <barjak.laszlo@gmail.com>
2018-02-10 00:19:54 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 'use strict';
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+let FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
     entry: './src/ui.js',
@@ -10,6 +11,7 @@ module.exports = {
     },
     plugins: [
         // Copy our app's index.html to the build folder.
-        new CopyWebpackPlugin([{ from: './src/index.html', to: 'index.html' }])
+        new CopyWebpackPlugin([{ from: './src/index.html', to: 'index.html' }]),
+        new FaviconsWebpackPlugin('./src/assets/augmint_logo_512.png')
     ]
 };
