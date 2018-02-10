a0e312a58fca1e20d15df89eb10834755143cf08
Peter Petrovics <szerintedmi@gmail.com>
2018-02-10 12:21:12 +0000

    Revert "Update webpack.config.js"
    This reverts commit 7d380afe39799e86bb112ec7e8b98b419aa472c9.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 'use strict';
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-let FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
     entry: './src/ui.js',
@@ -11,7 +10,6 @@ module.exports = {
     },
     plugins: [
         // Copy our app's index.html to the build folder.
-        new CopyWebpackPlugin([{ from: './src/index.html', to: 'index.html' }]),
-        new FaviconsWebpackPlugin('./src/assets/augmint_logo_512.png')
+        new CopyWebpackPlugin([{ from: './src/index.html', to: 'index.html' }])
     ]
 };
