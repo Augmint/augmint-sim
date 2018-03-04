a33aa0cd866995b38f8ece48954231ae531b885f
Peter Petrovics <szerintedmi@gmail.com>
2018-03-04 12:57:36 +0000

    use round() in Big constructors instead of div

--- src/lib/bigNums.js
+++ src/lib/bigNums.js
@@ -14,9 +14,9 @@ BigPt.DP = 6;
 BigPt.RM = 1;
 
 module.exports = {
-    BigAcd: n => new BigAcd(n).div(1), // to enforce decimalplaces
-    BigEth: n => BigEth(n).div(1), // to enforce decimalplaces,
-    BigPt: n => BigPt(n).div(1), // to enforce decimalplaces
+    BigAcd: n => new BigAcd(n).round(BigAcd.DP, BigAcd.RM), // to enforce decimalplaces
+    BigEth: n => BigEth(n).round(BigEth.DP, BigEth.RM), // to enforce decimalplaces,
+    BigPt: n => BigPt(n).round(BigPt.DP, BigPt.RM), // to enforce decimalplaces
     get ACD_DP() {
         return BigAcd.DP;
     },
